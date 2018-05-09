c23f3a9c79cc95a08cf7466fafb766b6c5fc729f
Qjaquier <q.jaquier@gmail.com>
2018-05-09 17:35:13 +0200

    Remove invoker test

--- coverageOutput/invoker/src/tsss/test.scala
+++
@@ -1,6 +0,0 @@
-object test {
-  def main(args: Array[String]) = {
-  //val file = new File("H:\\Github\\dotty\\coverageOutput\\", "aaa")
-	Invoker.invoked(3213, "H:\\Github\\dotty\\coverageOutput\\")
-  }
-}
